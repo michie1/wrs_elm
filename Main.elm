2fff7309e98fff96eed02a827aa92aa921373adf
Michiel <git@fastfox.nl>
2016-10-09 09:47:44 +0200

    Added material ui with elm-mdl package

--- Main.elm
+++ Main.elm
@@ -6,10 +6,18 @@ import Html.Events exposing (onInput)
 import Focus exposing (..)
 import Array exposing (Array, fromList, get)
 
+import Material
+import Material.Scheme
+import Material.Button as Button
+import Material.Options as Options exposing (css)
+import Material.Typography as Typo
+import Material.Table as Table
+
 main =
-  Html.beginnerProgram
-    { model = model
+  Html.program
+    { init = ( model, Cmd.none ) 
     , view = view
+    , subscriptions = always Sub.none 
     , update = update
     }
 
@@ -29,6 +37,7 @@ type alias Model =
   , bla : String
   , rider : Rider
   , races : Array Race
+  , mdl : Material.Model
   }
 
 
@@ -38,46 +47,63 @@ model =
     1 
     "foo"
     (Rider "Michiel" "Elite") 
-    (fromList [ 
-      Race "race a" 
-    , Race "race b" 
-    ])
+    (fromList [ Race "race a", Race "race b" ])
+    Material.model
 
 -- UPDATE
-
 
 type Msg
   = Increment
   | Decrement
   | Change
   | Add
+  | Mdl (Material.Msg Msg)
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-        { model | counter = model.counter + 1 }
+      ( { model | counter = model.counter + 1 }
+      , Cmd.none
+      )
 
     Decrement ->
-        { model | counter = model.counter - 1 }
+      ( { model | counter = model.counter - 1 }
+      , Cmd.none
+      )
 
     Change -> 
-        { model | bla = toString model.counter }
+      ( { model | bla = toString model.counter }
+      , Cmd.none
+      )
 
     Add -> 
-        { model | races = Array.push (Race "race c") model.races}
+      ( { model | races = Array.push (Race "race c") model.races}
+      , Cmd.none
+      )
+
+    Mdl msg' -> 
+      Material.update msg' model
 
 
 
 -- VIEW
 
+type alias Mdl = 
+  Material.Model 
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ Options.styled Html.p [ Typo.display3 ] [text "WRS"]
+    , button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model.counter) ]
     , button [ onClick Increment ] [ text "+" ]
+    , Button.render Mdl [0] model.mdl 
+      [ Button.onClick Increment 
+      , css "margin" "0 24px"
+      ]
+      [ text "Increase" ]
     , div [] 
       [ button [ onClick Change ] [ text model.bla ]
       , button [ onClick Add ] [ text "Add race c"]
@@ -85,8 +111,27 @@ view model =
       , div [] [ printMaybeRaceName (Array.get 0 model.races) ]
       , div [] [ showRaceNames model.races ]
       ]
+    , Table.table []
+      [ Table.thead []
+        [ Table.tr []
+          [ Table.th [] [ text "Naam" ]
+          , Table.th [ ] [ text "Quantity" ]
+          , Table.th [ ] [ text "Unit Price" ]
+          ]
+        ]
+      , Table.tbody []
+          (Array.toList model.races |> List.map (\item ->
+             Table.tr []
+               [ Table.td [] [ text item.name ]
+               , Table.td [ Table.numeric ] [ text item.name ]
+               , Table.td [ Table.numeric ] [ text item.name ]
+               ]
+             )
+          )
+      ]
     , viewRider model
     ]
+  |> Material.Scheme.top
 
 viewRider : Model -> Html msg
 viewRider model =
