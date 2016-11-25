7492bca410d5fa4e437f15e132ef773a7aa71b97
Michiel <michiel@blue-donut.com>
2016-11-25 21:09:08 +0100

    Translations

--- src/Races/List.elm
+++ src/Races/List.elm
@@ -1,28 +1,10 @@
 module Races.List exposing (..)
 
---import Races.Msg as Races exposing (Msg(..))
-
---import App.Model
 import Races.Model exposing (Race, categoryString)
 import Results.Model
-
-
---exposing (Mdl)
-
 import App.Msg
-
-
---import App.Page
-
 import Html exposing (Html, h2, div, text, a, table, tr, td, th, thead, tbody)
 import Html.Attributes exposing (href, class)
-
-
---import Material.Button as Button
-
---import Material.Options as Options exposing (Style, css)
---import Material.Typography as Typo
---import Material.Table as Table
 
 
 render : List Race -> List Results.Model.Result -> Html App.Msg.Msg
@@ -40,35 +22,35 @@ raceTable races results =
     table []
         [ thead []
             [ tr []
-                [ th [] [ text "Naam" ]
-                , th [] [ text "Datum" ]
-                , th [] [ text "Soort" ]
+                [ th [] [ text "Name" ]
+                , th [] [ text "Date" ]
+                , th [] [ text "Category" ]
                 , th [] [ text "# Participants" ]
                 ]
             ]
         , tbody []
-            (races
-                |> List.map
-                    (\race ->
-                        tr []
-                            [ td []
-                                [ a
-                                    [ href ("#races/" ++ (toString race.id)) ]
-                                    [ text race.name ]
-                                ]
-                            , td [] [ text race.date ]
-                            , td [] [ text (categoryString race.category) ]
-                            , td [ ] [ text (toString (countParticipants race.id results)) ]
+            (List.map
+                (\race ->
+                    tr []
+                        [ td []
+                            [ a
+                                [ href ("#races/" ++ (toString race.id)) ]
+                                [ text race.name ]
                             ]
-                    )
+                        , td [] [ text race.date ]
+                        , td [] [ text (categoryString race.category) ]
+                        , td [] [ text (toString (countParticipants race.id results)) ]
+                        ]
+                )
+                races
             )
         ]
 
+
 countParticipants : Int -> List Results.Model.Result -> Int
 countParticipants raceId results =
-    List.length 
-        ( List.filter
+    List.length
+        (List.filter
             (\result -> result.raceId == raceId)
             results
         )
-
