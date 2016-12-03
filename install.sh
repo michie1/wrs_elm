6557ec39230871781f0547390cf6c564523a11e1
Michiel <michiel@blue-donut.com>
2016-12-03 15:51:40 +0100

    rebase

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 elm-package install
-npm install --save elm-webpack-loader elm-hot-loader
+npm install --save webpack webpack-dev-server elm-webpack-loader elm-hot-loader
 npm install --save-dev clean-webpack-plugin copy-webpack-plugin
