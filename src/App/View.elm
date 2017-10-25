468f38f5b09320f57d111afdf67fc84f39bdbe0b
Michiel <michiel@blue-donut.com>
2017-10-25 07:59:37 +0200

    Sidebar with collection

--- src/App/View.elm
+++ src/App/View.elm
@@ -1,6 +1,6 @@
 module App.View exposing (render)
 
-import Html exposing (Html, h2, button, nav, div, text, span, a, input, ul, li, node)
+import Html exposing (Html, h2, h3, h4, button, nav, div, text, span, a, input, ul, li, node)
 import Html.Attributes exposing (attribute, href, id, class)
 import Html.Events exposing (onInput, onClick)
 import App.Msg exposing (Msg(..))
@@ -28,22 +28,26 @@ render app =
 mainView : App -> Html Msg
 mainView app =
     div [ class "container" ]
-        [ div [ class "row" ] 
+        [ div [ class "row" ]
             [ div [ class "col s3" ] [ sidebar app ]
             , div [ class "col s9" ] [ viewPage app ]
-            ] 
+            ]
         ]
+
 
 sidebar : App -> Html Msg
 sidebar app =
-    div [ ]
+    div []
         [ h2 [] [ text "Uitslagen" ]
-        , ul [] 
-            [ li [] [ a [ href "#races" ] [ text "Races" ] ]
-            , li [] [ a [ href "#riders" ] [ text "Riders" ] ]
+        , ul [ class "collection" ]
+            [ li [ class "collection-header" ] [ h4 [] [ a [ href "#races" ] [ text "Races" ] ] ]
+            , li [ ] [ a [ class "collection-item", href "#riders" ] [ text "Race 1" ] ]
+            , li [ class "collection-header" ] [ h4 [] [ a [ href "#races" ] [ text "Riders" ] ] ]
+            , li [ ] [ a [ class "collection-item", href "#riders" ] [ text "Rider 1" ] ]
+            , li [ ] [ a [ class "collection-item", href "#riders" ] [ text "Rider 1" ] ]
             ]
         ]
-    
+
 
 viewPage : App -> Html Msg
 viewPage app =
